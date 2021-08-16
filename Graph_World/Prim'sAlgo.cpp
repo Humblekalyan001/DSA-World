be632c8e92b789809faa2c9e10f1fe63ee323d01
Kalyan Chowdhury <74340429+Lazy-panda001@users.noreply.github.com>
2021-08-16 12:14:17 +0530

    Create Prim'sAlgo.cpp

---
+++ Graph_World/Prim'sAlgo.cpp
@@ -0,0 +1,205 @@
+// Hackerearth Implementation --> adjacency list representation
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <functional>
+#include <utility>
+
+using namespace std;
+const int MAX = 1e4 + 5;
+typedef pair<long long, int> PII;
+bool marked[MAX]; // -> precessed vectex
+vector <PII> adj[MAX];  // -> adjacency list
+
+long long prim(int src)
+{
+    priority_queue<PII, vector<PII>, greater<PII> > Q;
+    
+    long long minimumCost = 0;
+    PII p;
+    Q.push(make_pair(0, src));   // (weight, vertex) format
+    while(!Q.empty())
+    {
+        // Select the edge with minimum weight
+        p = Q.top();
+        Q.pop();
+       int x = p.second;
+        // Checking for cycle
+        if(marked[x] == true)
+            continue;
+        minimumCost += p.first;
+        marked[x] = true;
+        for(int i = 0;i < adj[x].size();++i)
+        {
+           int y = adj[x][i].second;
+            if(marked[y] == false)
+                Q.push(adj[x][i]);
+        }
+    }
+    return minimumCost;
+}
+
+int main()
+{
+    int nodes, edges, x, y;
+    long long weight, minimumCost;
+    cin >> nodes >> edges;
+    for(int i = 0;i < edges;++i)
+    {
+        cin >> x >> y >> weight;
+        adj[x].push_back(make_pair(weight, y));
+        adj[y].push_back(make_pair(weight, x));
+    }
+    // Selecting 1 as the starting node
+    minimumCost = prim(1);
+    cout << minimumCost << endl;
+    return 0;
+}
+
+//Time Complexity:
+"The time complexity of the Prim’s Algorithm is O((V+E)logV)  because each vertex is inserted in the priority queue only once and insertion in priority queue take logarithmic time"
+  
+*************************************************************************************************************************************************
+// Striver Adjacency List representation 
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int N,m;
+	cin >> N >> m;
+	vector<pair<int,int> > adj[N]; 
+
+	int a,b,wt;
+	for(int i = 0; i<m ; i++){
+		cin >> a >> b >> wt;
+		adj[a].push_back(make_pair(b,wt));
+		adj[b].push_back(make_pair(a,wt));
+	}	
+	
+	int parent[N]; 
+      
+    int key[N]; 
+      
+    bool mstSet[N]; 
+  
+    for (int i = 0; i < N; i++) 
+        key[i] = INT_MAX, mstSet[i] = false; 
+    
+    priority_queue< pair<int,int>, vector <pair<int,int>> , greater<pair<int,int>> > pq;
+
+    key[0] = 0; 
+    parent[0] = -1; 
+    pq.push({0, 0});
+    // Run the loop till all the nodes have been visited
+    // because in the brute code we checked for mstSet[node] == false while computing the minimum
+    // but here we simply take the minimal from the priority queue, so a lot of times a node might be taken twice
+    // hence its better to keep running till all the nodes have been taken. 
+    // try the following case: 
+    // Credits: Srejan Bera
+    // 6 7 
+    // 0 1 5 
+    // 0 2 10 
+    // 0 3 100 
+    // 1 3 50 
+    // 1 4 200
+    // 3 4 250
+    // 4 5 50 
+    while(!pq.empty())
+    { 
+        int u = pq.top().second; 
+        pq.pop(); 
+        
+        mstSet[u] = true; 
+        
+        for (auto it : adj[u]) {
+            int v = it.first;
+            int weight = it.second;
+            if (mstSet[v] == false && weight < key[v]) {
+                parent[v] = u;
+		key[v] = weight; 
+                pq.push({key[v], v});    
+            }
+        }
+            
+    } 
+    
+    for (int i = 1; i < N; i++) 
+        cout << parent[i] << " - " << i <<" \n"; 
+	return 0;
+}
+
+TC: O((V+E)logV)
+*************************************************************************************************************************************************
+
+// Tech Dose --> Adjacency List Representation
+#include<bits/stdc++.h>
+using namespace std;
+
+#define V 6		//No of vertices
+
+int selectMinVertex(vector<int>& value,vector<bool>& setMST)
+{
+	int minimum = INT_MAX;
+	int vertex;
+	for(int i=0;i<V;++i)
+	{
+		if(setMST[i]==false && value[i]<minimum)
+		{
+			vertex = i;
+			minimum = value[i];
+		}
+	}
+	return vertex;
+}
+
+void findMST(int graph[V][V])
+{
+	int parent[V];		//Stores MST
+	vector<int> value(V,INT_MAX);	//Used for edge relaxation
+	vector<bool> setMST(V,false);	//TRUE->Vertex is included in MST
+
+	//Assuming start point as Node-0
+	parent[0] = -1;	//Start node has no parent
+	value[0] = 0;	//start node has value=0 to get picked 1st
+
+	//Form MST with (V-1) edges
+	for(int i=0;i<V-1;++i)
+	{
+		//Select best Vertex by applying greedy method
+		int U = selectMinVertex(value,setMST);
+		setMST[U] = true;	//Include new Vertex in MST
+
+		//Relax adjacent vertices (not yet included in MST)
+		for(int j=0;j<V;++j)
+		{
+			/* 3 constraints to relax:-
+			      1.Edge is present from U to j.
+			      2.Vertex j is not included in MST
+			      3.Edge weight is smaller than current edge weight
+			*/
+			if(graph[U][j]!=0 && setMST[j]==false && graph[U][j]<value[j])
+			{
+				value[j] = graph[U][j];
+				parent[j] = U;
+			}
+		}
+	}
+	//Print MST
+	for(int i=1;i<V;++i)
+		cout<<"U->V: "<<parent[i]<<"->"<<i<<"  wt = "<<graph[parent[i]][i]<<"\n";
+}
+
+int main()
+{
+	int graph[V][V] = { {0, 4, 6, 0, 0, 0},
+						{4, 0, 6, 3, 4, 0},
+						{6, 6, 0, 1, 8, 0},
+						{0, 3, 1, 0, 2, 3},
+						{0, 4, 8, 2, 0, 7},
+						{0, 0, 0, 3, 7, 0} };
+
+	findMST(graph);	
+	return 0;
+}
+
+//TIME COMPLEXITY: O(V^2)
