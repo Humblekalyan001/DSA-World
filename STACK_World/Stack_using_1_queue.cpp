ef815f1cf32907cd26390c0ef4f40938e5a0c426
Kalyan Chowdhury <74340429+Lazy-panda001@users.noreply.github.com>
2021-11-02 12:49:47 +0530

    Update Stack_using_1_queue.cpp

--- STACK_World/Stack_using_1_queue.cpp
+++ STACK_World/Stack_using_1_queue.cpp
@@ -1,32 +1,66 @@
-void QueueStack :: push(int x)
-{
-        // Your Code
-        q1.push(x);
-        int size= q1.size()-1;
-        
-        while(size--)
-        {
-            q1.push(q1.front());
-            q1.pop();
-        }
-}
-
-//Function to pop an element from stack using two queues. 
-int QueueStack :: pop()
-{
-        // Your Code
-        int answer=-1;
-        if(q1.size() == 0)
-        {
-            return -1;
-        }
-        else
-        {
-            answer = q1.front();
-            q1.pop();
-        }
-        return answer;
-}
 
 TC :  O(n)
 SC : O(n)
+
+class MyStack {
+public:
+    /** Initialize your data structure here. */
+    queue<int> qu1;
+    MyStack() 
+    {
+
+    }
+    
+    /** Push element x onto stack. */
+    void push(int x) 
+    {
+       // step1: push into queue1
+        qu1.push(x);
+        
+        // step 2: take size of queue-1 and push all size elements again in queue from back
+        int size = qu1.size()-1;
+        
+        while(size--)
+        {
+            qu1.push(qu1.front());
+            qu1.pop();
+        }
+    }
+    
+    /** Removes the element on top of the stack and returns that element. */
+    int pop() 
+    {
+        // pop will be done from queue-2
+        int x = qu1.front();
+        qu1.pop();
+        return x;
+    }
+    
+    /** Get the top element. */
+    int top() 
+    {
+        return qu1.front();
+    }
+    
+    /** Returns whether the stack is empty. */
+    bool empty() 
+    {
+        if(qu1.size() == 0)
+        {
+            return true;
+        }
+        
+        else
+            return false;
+    }
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
+
